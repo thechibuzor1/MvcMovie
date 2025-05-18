878cca87e3de1f139d6465233e2d2e2b6ed228f9
Chibuzor <chibuzorm26@gmail.com>
2025-05-18 10:35:08 +0100

    Add Welcome action and update app branding
    - Implemented a new `Welcome` action in `HelloWorldController.cs` for greeting users.
    - Changed application title and navbar branding from "MvcMovie" to "Movie App" in `_Layout.cshtml`.
    - Updated footer in `_Layout.cshtml` to reflect new application name.
    - Enhanced `Index.cshtml` with additional content and a page title.

--- Controllers/HelloWorldContoller.cs
+++ Controllers/HelloWorldContoller.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
+        // GET  method : /HelloWorld
         public IActionResult Index()
         {
             return View();
         }
+
+        // GET  method : /HelloWorld/Welcome
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+                
+        }
+
+
+
+        //Controller/ActionName/Parameter
     }
 }
