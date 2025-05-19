67acce7623b013505d13a1f0bfe864afc9117b41
Chibuzor <chibuzorm26@gmail.com>
2025-05-19 14:39:42 +0100

    Add Rating property to Movie model and update views
    - Introduced a new required `Rating` property in the `Movie` model with a maximum length of 5 characters.
    - Updated `Create` and `Edit` action methods in `MoviesController` to bind the new `Rating` property.
    - Added a migration to include the `Rating` column in the `Movie` table.
    - Modified views (`Create.cshtml`, `Edit.cshtml`, `Details.cshtml`, `Delete.cshtml`, and `Index.cshtml`) to support the new `Rating` property.
    - Enhanced navigation by adding a link to the Movies index page in `_Layout.cshtml`.

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -25,56 +25,64 @@ namespace MvcMovie.Models
                             Title = "When Harry Met Sally",
                             ReleaseDate = DateTime.Parse("1989-1-11"),
                             Genre = "Romantic Comedy",
-                            Price = 7.99M
+                            Price = 7.99M,
+                            Rating = "PG"
                     },
                     new Movie
                     {
                             Title = "Ghostbusters ",
                             ReleaseDate = DateTime.Parse("1984-3-13"),
                             Genre = "Comedy",
-                            Price = 8.99M
+                            Price = 8.99M,
+                            Rating = "PG"
                     },
                     new Movie
                     {
                             Title = "Ghostbusters 2",
                             ReleaseDate = DateTime.Parse("1986-2-23"),
                             Genre = "Comedy",
-                            Price = 9.99M
+                            Price = 9.99M,
+                            Rating = "M"
                     },
                     new Movie
                     {
                             Title = "Rio Bravo",
                             ReleaseDate = DateTime.Parse("1959-4-15"),
                             Genre = "Western",
-                            Price = 3.99M
+                            Price = 3.99M,
+
                     },
                     new Movie
                     {
                             Title = "The Shawshank Redemption",
                             ReleaseDate = DateTime.Parse("1994-9-23"),
                             Genre = "Drama",
-                            Price = 9.99M
+                            Price = 9.99M,
+                            Rating = "M"
                     },
                     new Movie
                     {
                             Title = "The Godfather",
                             ReleaseDate = DateTime.Parse("1972-3-24"),
                             Genre = "Drama",
-                            Price = 12.99M
+                            Price = 12.99M,
+                            Rating = "PG"
                     },
                     new Movie
                     {
                             Title = "The Godfather, Part II",
                             ReleaseDate = DateTime.Parse("1974-12-20"),
                             Genre = "Drama",
-                            Price = 10.99M
+                            Price = 10.99M,
+                            Rating = "PG"
                     },
                     new Movie
                     {
                             Title = "The Dark Knight",
                             ReleaseDate = DateTime.Parse("2008-7-18"),
                             Genre = "Action",
-                            Price = 15.99M
+                            Price = 15.99M,
+                            Rating = "PG"
                     }
                     );
 
